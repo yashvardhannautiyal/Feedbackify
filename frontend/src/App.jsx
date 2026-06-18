70d1196ed4219d5495b478bd815715181706be1c
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-06-18 22:35:31 +0530

    routes defined

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'; 
 import HomePage from './pages/HomePage/HomePage'
+import Login from './pages/UserRegister/Login';
+import SignIn from './pages/UserRegister/SignIn';
+import Dashboard from './pages/Dashboard/Dashboard';
+
 
 function App() {
   return (
-    <div className='bg-orange-50'>
-      <HomePage />
+    <div className="bg-orange-50 min-h-screen">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
