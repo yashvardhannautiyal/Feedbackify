85fecf3abf6b74b1c480f2e3445216d408cb7db6
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-06-20 02:50:55 +0530

    feedbackForm and item UI added

--- frontend/src/components/FeedbackItem.jsx
+++ frontend/src/components/FeedbackItem.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { CiMail } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 function Feedback() {
   return (
-    <div>
-      this is feedback
+    <div className="px-30 pb-10">
+      <p className="text-gray-700">Showing 2 entries</p>
+      <div className="bg-white border-yellow-800 shadow-lg rounded-xl p-8 mt-2 px-10">
+       {/* user details  */}
+        <div className="flex justify-between items-center">
+
+          {/* name + email  */}
+          <div>
+            <p className='flex gap-1 items-center font-bold text-lg'><CiUser />Michael Chen</p>
+            <p className='flex gap-1 items-center text-sm text-gray-600'><CiMail /> mchen@gmail.com</p>
+          </div>
+
+          {/* date + delete button  */}
+          <div className="flex gap-4 items-center">
+            <div className="bg-orange-50 rounded-xl px-2"><p>Jun 12, 2026</p></div>
+            <button className="text-red-700 hover:bg-red-100 hover:cursor-pointer p-1 rounded-sm"><RiDeleteBinLine /></button>
+          </div>
+        </div>
+
+        {/* feedback  */}
+        <div className="pt-7">
+          <p>The user interface could be more intuitive. Consider adding more tooltips.</p>
+          <p className="pt-4 text-gray-600 text-sm">Submitted on Jun 12, 2026, 05:30 AM</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Feedback
+export default Feedback;
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
 
 function SearchBar() {
   return (
     <div className='px-30 pt-10'>
       <hr className="text-gray-300 " />
+
+      <div className='bg-white border-yellow-800 shadow-lg rounded-xl p-8 my-10'>
+        <p className='flex gap-1 items-center roman-font'><CiSearch /> FILTER</p>
+
+        {/* filter input  */}
+        <div className='flex justify-between px-5 pt-3'>
+        <div className='w-xl'>
+          <p className='text-xs font-bold pb-1 text-gray-700'>SEARCH</p>
+          <input type="text" placeholder='Name, email, or message....' className="rounded-lg w-full py-1 px-2 bg-orange-50 border border-yellow-700 outline-yellow-900"/>
+        </div>
+        <div className='w-xl'>
+          <p className='text-xs font-bold pb-1 text-gray-700'>FILTER BY DATE</p>
+          <input type="date" className="rounded-lg w-full py-1 px-2 bg-orange-50 border border-yellow-700 outline-yellow-900"/>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
